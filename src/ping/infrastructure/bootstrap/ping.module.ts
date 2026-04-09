f6e48439b18a65a32916bbb56f2b5c9d8615d4b6
Gustavo Palacios <gustavoalonso1997@hotmail.com>
2026-04-08 22:35:32 -0500

    feat: Agregando configuración para el arquetipo.

--- src/ping/infrastructure/bootstrap/ping.module.ts
+++ src/ping/infrastructure/bootstrap/ping.module.ts
@@ -1,19 +1,34 @@
 import { Module } from '@nestjs/common';
-import { PingController } from '../controller/ping.controller';
-import { PingUseCase } from '../../application/use-cases/ping.usecase';
-import { PingService } from '../../domain/service/ping.service';
-import { PingRepository } from '../../domain/repository/ping.repository';
-import { PingRepositoryImpl } from '../repository/ping.repository.impl';
 import { EnvValidationMiddleware } from '../../../common/middleware/env-validation.middleware';
 import { EnvConstants } from '../../../common/constants/env.constants';
+import { PingRepository } from '../../domain/repository/ping.repository';
+import { PingRepositoryImpl } from '../repository/ping.repository.impl';
+import { PingService } from '../../domain/service/ping.service';
+import { PingUseCase } from '../../application/use-cases/ping.usecase';
+import { PingController } from '../controller/ping.controller';
 
 @Module({
   providers: [
-    PingController,
-    PingUseCase,
-    PingService,
     EnvValidationMiddleware.register(EnvConstants.REQUERIDAS_PING),
-    { provide: PingRepository, useClass: PingRepositoryImpl },
+    {
+      provide: PingRepository,
+      useFactory: () => new PingRepositoryImpl(),
+    },
+    {
+      provide: PingService,
+      useFactory: (repo: PingRepository) => new PingService(repo),
+      inject: [PingRepository],
+    },
+    {
+      provide: PingUseCase,
+      useFactory: (svc: PingService) => new PingUseCase(svc),
+      inject: [PingService],
+    },
+    {
+      provide: PingController,
+      useFactory: (uc: PingUseCase) => new PingController(uc),
+      inject: [PingUseCase],
+    },
   ],
 })
 export class PingModule {}
